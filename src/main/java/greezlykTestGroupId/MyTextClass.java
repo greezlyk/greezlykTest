5b94dbf3a0b67d7f13ee159008d55e32de655fc5
greezlyk <greezlyk@yahoo.com>
2015-02-24 22:25:27 +0100

    first commit

--- src/main/java/greezlykTestGroupId/MyTextClass.java
+++ src/main/java/greezlykTestGroupId/MyTextClass.java
@@ -4,7 +4,9 @@ public class MyTextClass {
 
 	
 	public String getRandomNumber() {
-	
+	 
+		
+		 sdfasf
 		final float minimum = 1;
 		final float maximum = 1000; 
 		return String.valueOf(minimum + (int)(Math.random()*maximum));	

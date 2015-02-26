ed2ca7538a8e28de022b2c282100367525daad67
Greezlyk <greezlyk@yahoo.com>
2015-02-26 11:38:22 +0100

    Update MyTextClass.java

--- src/main/java/greezlykTestGroupId/MyTextClass.java
+++ src/main/java/greezlykTestGroupId/MyTextClass.java
@@ -9,6 +9,9 @@ public class MyTextClass {
 		
 		final float minimum = 1;
 		final float maximum = 1000; 
+		
+		adfasdf
+		
 		return String.valueOf(minimum + (int)(Math.random()*maximum));	
 		
 	}
